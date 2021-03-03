7d205b3a1ebaaa6f77a6e5934f9322f6fb8c0bf3
BenStigsen <benjamin.stigsen@gmail.com>
2021-03-03 15:23:04 +0100

    Added pygame window

--- chess.py
+++ chess.py
@@ -1,25 +1,60 @@
 import draw
 import board
+import pygame
 #import piece
+
+screen = None
+
+squareSize = 55
 
 class Chess:
   def __init__(self):
     self.board = board.Board()
 
   def run(self):
-    self.board.draw()
-    moves = self.board.getPiece((0, 1)).getPossibleMoves()
-    print(self.board.sanitizeMoves(moves))
+    self.running = True
+    
+    while self.running:
+      draw = self._update()
+      
+      if (draw):
+        self._draw()
+    
+    
+      #_update()
+      #_draw()  
   
   def _update(self):
+    events = pygame.event.get()
+    
+    for event in events:
+      self.running = (event.type != pygame.QUIT)
+      
+      if (event.type == pygame.MOUSEBUTTONDOWN):
+        if (event.button == 1): # Left click
+          pos = pygame.mouse.get_pos()
+          
+          row = pos[0] // squareSize
+          col = pos[1] // squareSize
+  
+          self.board.draw()
+    #moves = self.board.getPiece((0, 1)).getPossibleMoves()
+    #print(self.board.sanitizeMoves(moves))
     # Update here (input, moving)
     pass
     
   def _draw(self):
-    # Draw board here
+    # Draw board
+    pygame.draw.rect()
+    pass
+    
+  def _getInput(self):
     pass
     
 if __name__ == "__main__":
+  pygame.init()
+  screen = pygame.display.set_mode([500, 500])
+  
   game = Chess()
   game.run()
 
